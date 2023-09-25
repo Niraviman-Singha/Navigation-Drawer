feb3575a4f07b4c52aaba12271f3461fbabf5e0d
niraviman-singha <niraviman15-11689@diu.edu.bd>
2023-09-25 19:30:43 +0600

    DrawerLayout, FrameLayout, BottomNavigationView

---
+++ app/src/main/java/com/example/naigationdrawer/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.naigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.naigationdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    val homeFragment = HomeFragment()
+    val personFragment = PersonFragment()
+    val settingFragment = SettingFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setCurrentFragment(homeFragment)
+
+        binding.navView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->{
+                    setCurrentFragment(homeFragment)
+                    binding.drawerLayout.closeDrawers()
+                }
+
+                R.id.person ->{
+                    setCurrentFragment(personFragment)
+                    binding.drawerLayout.closeDrawers()
+                }
+
+                R.id.setting ->{
+                    setCurrentFragment(settingFragment)
+                    binding.drawerLayout.closeDrawers()
+                }
+            }
+            true
+        }
+
+
+        binding.bottomBar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->setCurrentFragment(homeFragment)
+                R.id.person ->setCurrentFragment(personFragment)
+                R.id.setting ->setCurrentFragment(settingFragment)
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
+        }
+        else super.onOptionsItemSelected(item)
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, fragment)
+            commit()
+        }
+
+
+
+    }
+}
